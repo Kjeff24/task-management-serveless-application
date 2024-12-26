24f3dcf3f2e8f05c7aac957e916dfd9810955b1e
Kjeff24 <jeffreyarthur123@gmail.com>
2024-12-26 13:19:21 +0000

    Ensure when user is created, subscription message is sent

--- src/main/java/org/example/service/impl/UserManagementServiceImpl.java
+++ src/main/java/org/example/service/impl/UserManagementServiceImpl.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
 import software.amazon.awssdk.services.cognitoidentityprovider.model.*;
+import software.amazon.awssdk.services.sns.SnsClient;
+import software.amazon.awssdk.services.sns.model.SubscribeRequest;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,10 +18,19 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class UserManagementServiceImpl implements UserManagementService {
-
+    private final SnsClient snsClient;
     private final CognitoIdentityProviderClient cognitoIdentityProviderClient;
     @Value("${app.aws.cognito.user.pool.id}")
     private String userPoolId;
+    @Value("${app.aws.sns.topics.tasks.assignment.arn}")
+    private String tasksAssignmentTopicArn;
+    @Value("${app.aws.sns.topics.tasks.reopened.arn}")
+    private String tasksReopenedTopicArn;
+    @Value("${app.aws.sns.topics.tasks.closed.arn}")
+    private String tasksClosedTopicArn;
+    @Value("${app.aws.sns.topics.tasks.deadline.arn}")
+    private String tasksDeadlineTopicArn;
+
 
     public MessageResponse createUser(UserRequest userRequest) {
         List<AttributeType> userAttributes = new ArrayList<>();
@@ -28,12 +39,18 @@ public class UserManagementServiceImpl implements UserManagementService {
 
         AdminCreateUserRequest createUserRequest = AdminCreateUserRequest.builder()
                 .userPoolId(userPoolId)
-                .username(userRequest.username())
+                .username(userRequest.email())
                 .userAttributes(userAttributes)
                 .temporaryPassword(PasswordGenerator.generatePassword(9))
                 .desiredDeliveryMediums(DeliveryMediumType.EMAIL)
                 .build();
         cognitoIdentityProviderClient.adminCreateUser(createUserRequest);
+
+        subscribeUserToSNSTopic(tasksAssignmentTopicArn, userRequest.email());
+        subscribeUserToSNSTopic(tasksClosedTopicArn, userRequest.email());
+        subscribeUserToSNSTopic(tasksReopenedTopicArn, userRequest.email());
+        subscribeUserToSNSTopic(tasksDeadlineTopicArn, userRequest.email());
+
         return MessageResponse.builder().message("User creation successful").build();
     }
 
@@ -57,5 +74,15 @@ public class UserManagementServiceImpl implements UserManagementService {
         } while (paginationToken != null);
 
         return allUsers;
+    }
+
+    private void subscribeUserToSNSTopic(String topicArn, String email) {
+        SubscribeRequest request = SubscribeRequest.builder()
+                .topicArn(topicArn)
+                .protocol("email")
+                .endpoint(email)
+                .build();
+
+        snsClient.subscribe(request);
     }
 }
